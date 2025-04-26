4e11156de2cd8d60a5522aa59171654ef4510201
xoraxd <fordocumentspost@yandex.ru>
2025-04-26 21:39:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const path = require('path');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 
 const jwt = require('jsonwebtoken')
